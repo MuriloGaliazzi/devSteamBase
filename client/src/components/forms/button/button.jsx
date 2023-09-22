a2cf1b0c193eb13ef35e11ed41584c5c516a8641
galiazzi-17 <murilo.galiazzi@gmail.com>
2023-09-22 15:38:23 -0300

    feito o saleCard

---
+++ client/src/components/forms/button/button.jsx
@@ -0,0 +1,11 @@
+import styles from "./button.module.css";
+
+const Button = ({ children, fullwidth }) => {
+  return (
+    <div className={`${styles.button} ${fullwidth && styles.fullwidth}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Button;
